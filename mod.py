0f7ca9e3fc210aeb5b8ff47c07a4711a1a9f9deb
gvielza <voltagnelly@gmail.com>
2023-10-04 16:11:03 +0000

    agrgando modularizacion

---
+++ mod.py
@@ -0,0 +1,13 @@
+#import math
+#print(math.sqrt(25))
+#print(math.fabs(-24.5))
+
+#from math import sqrt
+#print(sqrt(9))
+#print(fabs(-25))
+
+#import math as m
+#print(m.sqrt(19))
+
+from math import sqrt as sq
+print(sq(45))
